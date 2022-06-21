50aa05d1d3d19bf1154b4afd3763477fad3e862b
Anis Singh <anissingh@Aniss-MacBook-Pro.local>
2022-06-21 15:24:21 -0400

    Condensed similar code into less methods in BoardManagerImpl.java

--- src/com/anis/usecases/BoardManagerImpl.java
+++ src/com/anis/usecases/BoardManagerImpl.java
@@ -1,6 +1,8 @@
 package com.anis.usecases;
 
 import com.anis.entities.Tile;
+import com.anis.enums.Direction;
+
 import java.util.Random;
 
 public class BoardManagerImpl implements BoardManager {
@@ -88,71 +90,9 @@ public class BoardManagerImpl implements BoardManager {
 		boolean tileMoved = false;
 		for(int i = 0; i < ROWS; i++) {
 			// Merge all appropriate tiles
-			boolean tileSet = false;
-			int currIndex = 0;
-			// TODO: Move to helper?
-			for(int j = 0; j < COLS; j++) {
-				int tileBeingAnalyzedVal = TileManager.getTileValue(board[i][currIndex]);
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				
-				// Reset the prevX and prevY of the tile from last movement
-				TileManager.setTilePrevX(board[i][j], j);
-				TileManager.setTilePrevY(board[i][j], i);
-				
-				// If we aren't analyzing a tile yet and we find our first non-zero tile, analyze that tile
-				if(!tileSet && currTileVal != 0) {
-					tileSet = true;
-					currIndex = j;
-				} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
-					// If we are analyzing a tile and it has the same value as the next non-zero tile in the row,
-					// merge them
-					// Note tileSet being true ensures board[i][currIndex] value != 0 and therefore board[i][j].getValue() != 0
-					// TODO: Use TileManager here
-					board[i][currIndex].setValue(tileBeingAnalyzedVal * 2);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][currIndex], currIndex);
-					TileManager.setTilePrevY(board[i][currIndex], i);
-					// This is the tile that was merged, so set its wasMerged attribute to true
-					TileManager.setTileWasMerged(board[i][currIndex], true);
-					// Also set the tile to be expanding
-					TileManager.setTileIsExpanding(board[i][currIndex], true);
-					board[i][j].setValue(0);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					tileSet = false;
-					tileMoved = true;
-				} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
-					// If we are analyzing a tile and the next non-zero tile in the row does not have the same value, then
-					// stop analyzing this tile because it can't be merged and start analyzing the next one
-					// Note tileSet is already true so there is no need to set it here
-					currIndex = j;
-				}
-			}
+			tileMoved = handleLateralMerge(i, Direction.LEFT, tileMoved);
 			// Move all tiles to leftmost spot
-			currIndex = 0;
-			for(int j = 0; j < COLS; j++) {
-				// Check if board[i][j] tile value is non-zero
-				// If it is, set the leftmost free index to the tile at board[i][j]
-				// If the leftmost free index is not j, replace tile at board[i][j] with an empty tile because
-				// it was moved to another location
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				if(currTileVal != 0 && currIndex != j) {
-					TileManager.setTileValue(board[i][currIndex], currTileVal);
-					TileManager.setTilePrevX(board[i][currIndex], j);
-					TileManager.setTilePrevY(board[i][currIndex], i);
-					// board[i][currIndex] = board[i][j];
-					TileManager.setTileValue(board[i][j], 0);
-					// Don't want to animate an empty tile sliding
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					currIndex++;
-					tileMoved = true;
-				} else if(currTileVal != 0) {
-					// currIndex == j here
-					currIndex++;
-				}
-			}
+			tileMoved = handleLateralShifting(i, Direction.LEFT, tileMoved);
 		}
 		
 		// Return whether or not the tile moved
@@ -164,57 +104,10 @@ public class BoardManagerImpl implements BoardManager {
 		boolean tileMoved = false;
 		for(int i = 0; i < ROWS; i++) {
 			// Merge all appropriate tiles
-			boolean tileSet = false;
-			int currIndex = 0;
-			for(int j = COLS - 1; j >= 0; j--) {
-				int tileBeingAnalyzedVal = TileManager.getTileValue(board[i][currIndex]);
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				TileManager.setTilePrevX(board[i][j], j);
-				TileManager.setTilePrevY(board[i][j], i);
-				
-				if(!tileSet && currTileVal != 0) {
-					tileSet = true;
-					currIndex = j;
-				} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
-					// TODO: Use TileManager here
-					board[i][currIndex].setValue(tileBeingAnalyzedVal * 2);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][currIndex], currIndex);
-					TileManager.setTilePrevY(board[i][currIndex], i);
-
-					TileManager.setTileWasMerged(board[i][currIndex], true);
-					TileManager.setTileIsExpanding(board[i][currIndex], true);
-					
-					board[i][j].setValue(0);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					tileSet = false;
-					tileMoved = true;
-				} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
-					currIndex = j;
-				}
-			}
+			tileMoved = handleLateralMerge(i, Direction.RIGHT, tileMoved);
 			// Move all tiles to rightmost spot
-			currIndex = COLS - 1;
-			for(int j = COLS - 1; j >= 0; j--) {
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				if(currTileVal != 0 && currIndex != j) {
-					TileManager.setTileValue(board[i][currIndex], currTileVal);
-					TileManager.setTilePrevX(board[i][currIndex], j);
-					TileManager.setTilePrevY(board[i][currIndex], i);
-					TileManager.setTileValue(board[i][j], 0);
-					// Don't want to animate an empty tile sliding
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					currIndex--;
-					tileMoved = true;
-				} else if(currTileVal != 0) {
-					currIndex--;
-				}
-			}
+			tileMoved = handleLateralShifting(i, Direction.RIGHT, tileMoved);
 		}
-		
 		return tileMoved;
 	}
 	
@@ -223,56 +116,10 @@ public class BoardManagerImpl implements BoardManager {
 		boolean tileMoved = false;
 		for(int j = 0; j < COLS; j++) {
 			// Merge all appropriate tiles
-			boolean tileSet = false;
-			int currIndex = 0;
-			for(int i = 0; i < ROWS; i++) {
-				int tileBeingAnalyzedVal = TileManager.getTileValue(board[currIndex][j]);
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				TileManager.setTilePrevX(board[i][j], j);
-				TileManager.setTilePrevY(board[i][j], i);
-				
-				if(!tileSet && currTileVal != 0) {
-					tileSet = true;
-					currIndex = i;
-				} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
-					// TODO: Use TileManager here
-					board[currIndex][j].setValue(tileBeingAnalyzedVal * 2);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[currIndex][j], j);
-					TileManager.setTilePrevY(board[currIndex][j], currIndex);
-
-					TileManager.setTileWasMerged(board[currIndex][j], true);
-					TileManager.setTileIsExpanding(board[currIndex][j], true);
-					
-					board[i][j].setValue(0);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					tileSet = false;
-					tileMoved = true;
-				} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
-					currIndex = i;
-				}
-			}
+			tileMoved = handleVerticalMerging(j, Direction.UP, tileMoved);
 			
-			// Move all tiles to leftmost spot
-			currIndex = 0;
-			for(int i = 0; i < ROWS; i++) {
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				if(currTileVal != 0 && currIndex != i) {
-					TileManager.setTileValue(board[currIndex][j], currTileVal);
-					TileManager.setTilePrevX(board[currIndex][j], j);
-					TileManager.setTilePrevY(board[currIndex][j], i);
-					TileManager.setTileValue(board[i][j], 0);
-					// Don't want to animate an empty tile sliding
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					currIndex++;
-					tileMoved = true;
-				} else if(currTileVal != 0) {
-					currIndex++;
-				}
-			}
+			// Move all tiles to up-most spot
+			tileMoved = handleVerticalShifting(j, Direction.UP, tileMoved);
 		}
 		
 		// Return whether or not a tile moved
@@ -284,58 +131,180 @@ public class BoardManagerImpl implements BoardManager {
 		boolean tileMoved = false;
 		for(int j = 0; j < COLS; j++) {
 			// Merge all appropriate tiles
-			boolean tileSet = false;
-			int currIndex = 0;
-			for(int i = ROWS - 1; i >= 0; i--) {
-				int tileBeingAnalyzedVal = TileManager.getTileValue(board[currIndex][j]);
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				TileManager.setTilePrevX(board[i][j], j);
-				TileManager.setTilePrevY(board[i][j], i);
-				
-				if(!tileSet && currTileVal != 0) {
-					tileSet = true;
-					currIndex = i;
-				} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
-					// TODO: Use TileManager here
-					board[currIndex][j].setValue(tileBeingAnalyzedVal * 2);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[currIndex][j], j);
-					TileManager.setTilePrevY(board[currIndex][j], currIndex);
-
-					TileManager.setTileWasMerged(board[currIndex][j], true);
-					TileManager.setTileIsExpanding(board[currIndex][j], true);
-					
-					board[i][j].setValue(0);
-					// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					tileSet = false;
-					tileMoved = true;
-				} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
-					currIndex = i;
-				}
-			}
-			// Move all tiles to rightmost spot
-			currIndex = COLS - 1;
-			for(int i = COLS - 1; i >= 0; i--) {
-				int currTileVal = TileManager.getTileValue(board[i][j]);
-				if(currTileVal != 0 && currIndex != i) {
-					TileManager.setTileValue(board[currIndex][j], currTileVal);
-					TileManager.setTilePrevX(board[currIndex][j], j);
-					TileManager.setTilePrevY(board[currIndex][j], i);
-					TileManager.setTileValue(board[i][j], 0);
-					// Don't want to animate an empty tile sliding
-					TileManager.setTilePrevX(board[i][j], j);
-					TileManager.setTilePrevY(board[i][j], i);
-					currIndex--;
-					tileMoved = true;
-				} else if(currTileVal != 0) {
-					currIndex--;
-				}
-			}
+			tileMoved = handleVerticalMerging(j, Direction.DOWN, tileMoved);
+			
+			// Move all tiles to down-most spot
+			tileMoved = handleVerticalShifting(j, Direction.DOWN, tileMoved);
 		}
 		
 		// Return whether or not a tile moved
+		return tileMoved;
+	}
+	
+	// Precondition: direction is either Direction.LEFT or Direction.RIGHT
+	private boolean handleLateralMerge(int i, Direction direction, boolean tileMoved) {
+		boolean tileSet = false;
+		// currIndex = {0 if LEFT, COLS - 1 if RIGHT}
+		int currIndex = direction == Direction.LEFT ? 0 : COLS - 1;
+		// j = {0 if LEFT, COLS - 1 if RIGHT}
+		int j = direction == Direction.LEFT ? 0 : COLS - 1;
+		// update = {1 if LEFT, -1 if RIGHT}
+		int update = direction == Direction.LEFT ? 1 : -1;
+		
+		for(int unused = 0; unused < COLS; unused++) {
+			int tileBeingAnalyzedVal = TileManager.getTileValue(board[i][currIndex]);
+			int currTileVal = TileManager.getTileValue(board[i][j]);
+			
+			// Reset the prevX and prevY of the tile from last movement
+			TileManager.setTilePrevX(board[i][j], j);
+			TileManager.setTilePrevY(board[i][j], i);
+			
+			// If we aren't analyzing a tile yet and we find our first non-zero tile, analyze that tile
+			if(!tileSet && currTileVal != 0) {
+				tileSet = true;
+				currIndex = j;
+			} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
+				// If we are analyzing a tile and it has the same value as the next non-zero tile in the row,
+				// merge them
+				// Note tileSet being true ensures board[i][currIndex] value != 0 and therefore board[i][j].getValue() != 0
+				// TODO: Use TileManager here
+				board[i][currIndex].setValue(tileBeingAnalyzedVal * 2);
+				// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
+				TileManager.setTilePrevX(board[i][currIndex], currIndex);
+				TileManager.setTilePrevY(board[i][currIndex], i);
+				// This is the tile that was merged, so set its wasMerged attribute to true
+				TileManager.setTileWasMerged(board[i][currIndex], true);
+				// Also set the tile to be expanding
+				TileManager.setTileIsExpanding(board[i][currIndex], true);
+				board[i][j].setValue(0);
+				// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
+				TileManager.setTilePrevX(board[i][j], j);
+				TileManager.setTilePrevY(board[i][j], i);
+				tileSet = false;
+				tileMoved = true;
+			} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
+				// If we are analyzing a tile and the next non-zero tile in the row does not have the same value, then
+				// stop analyzing this tile because it can't be merged and start analyzing the next one
+				// Note tileSet is already true so there is no need to set it here
+				currIndex = j;
+			}
+			// Update j
+			j += update;
+		}
+		return tileMoved;
+	}
+	
+	// Precondition: direction is either Direction.UP or Direction.DOWN
+	private boolean handleVerticalMerging(int j, Direction direction, boolean tileMoved) {
+		boolean tileSet = false;
+		// currIndex = {0 if UP, ROWS - 1 if DOWN}
+		int currIndex = direction == Direction.UP ? 0 : ROWS - 1;
+		// i = {0 if UP, ROWS - 1 if DOWN}
+		int i = direction == Direction.UP ? 0 : ROWS - 1;
+		// update = {1 if UP, -1 if DOWN}
+		int update = direction == Direction.UP ? 1 : -1;
+		
+		for(int unused = 0; unused < ROWS; unused++) {
+			int tileBeingAnalyzedVal = TileManager.getTileValue(board[currIndex][j]);
+			int currTileVal = TileManager.getTileValue(board[i][j]);
+			TileManager.setTilePrevX(board[i][j], j);
+			TileManager.setTilePrevY(board[i][j], i);
+			
+			if(!tileSet && currTileVal != 0) {
+				tileSet = true;
+				currIndex = i;
+			} else if(tileSet && tileBeingAnalyzedVal == currTileVal) {
+				// TODO: Use TileManager here
+				board[currIndex][j].setValue(tileBeingAnalyzedVal * 2);
+				// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
+				TileManager.setTilePrevX(board[currIndex][j], j);
+				TileManager.setTilePrevY(board[currIndex][j], currIndex);
+
+				TileManager.setTileWasMerged(board[currIndex][j], true);
+				TileManager.setTileIsExpanding(board[currIndex][j], true);
+				
+				board[i][j].setValue(0);
+				// TODO: Do animations for merging. For now they are disabled by the following 2 lines of code
+				TileManager.setTilePrevX(board[i][j], j);
+				TileManager.setTilePrevY(board[i][j], i);
+				tileSet = false;
+				tileMoved = true;
+			} else if(tileSet && tileBeingAnalyzedVal != currTileVal && currTileVal != 0) {
+				currIndex = i;
+			}
+			// Update i
+			i += update;
+		}
+		
+		return tileMoved;
+	}
+	
+	// Precondition: direction is either Direction.LEFT or Direction.RIGHT
+	// Comments describe shifting left, but shifting right follows analogous logic
+	private boolean handleLateralShifting(int i, Direction direction, boolean tileMoved) {
+		// currIndex = {0 if LEFT, COLS - 1 if RIGHT}
+		int currIndex = direction == Direction.LEFT ? 0 : COLS - 1;
+		// j = {0 if LEFT, COLS - 1 if RIGHT}
+		int j = direction == Direction.LEFT ? 0 : COLS - 1;
+		// update = {1 if LEFT, -1 if RIGHT}
+		int update = direction == Direction.LEFT ? 1 : -1;
+		
+		for(int unused = 0; unused < COLS; unused++) {
+			// Check if board[i][j] tile value is non-zero
+			// If it is, set the leftmost free index to the tile at board[i][j]
+			// If the leftmost free index is not j, replace tile at board[i][j] with an empty tile because
+			// it was moved to another location
+			int currTileVal = TileManager.getTileValue(board[i][j]);
+			if(currTileVal != 0 && currIndex != j) {
+				TileManager.setTileValue(board[i][currIndex], currTileVal);
+				TileManager.setTilePrevX(board[i][currIndex], j);
+				TileManager.setTilePrevY(board[i][currIndex], i);
+				// board[i][currIndex] = board[i][j];
+				TileManager.setTileValue(board[i][j], 0);
+				// Don't want to animate an empty tile sliding
+				TileManager.setTilePrevX(board[i][j], j);
+				TileManager.setTilePrevY(board[i][j], i);
+				currIndex += update;
+				tileMoved = true;
+			} else if(currTileVal != 0) {
+				// currIndex == j here
+				currIndex += update;
+			}
+			// Update j
+			j += update;
+		}
+		
+		return tileMoved;
+	}
+	
+	// Precondition: direction is either Direction.UP or Direction.DOWN
+	private boolean handleVerticalShifting(int j, Direction direction, boolean tileMoved) {
+		// currIndex = {0 if UP, ROWS - 1 if DOWN}
+		int currIndex = direction == Direction.UP ? 0 : ROWS - 1;
+		// i = {0 if UP, ROWS - 1 if DOWN}
+		int i = direction == Direction.UP ? 0 : ROWS - 1;
+		// update = {1 if UP, -1 if DOWN}
+		int update = direction == Direction.UP ? 1 : -1;
+		
+		for(int unused = 0; unused < ROWS; unused++) {
+			int currTileVal = TileManager.getTileValue(board[i][j]);
+			if(currTileVal != 0 && currIndex != i) {
+				TileManager.setTileValue(board[currIndex][j], currTileVal);
+				TileManager.setTilePrevX(board[currIndex][j], j);
+				TileManager.setTilePrevY(board[currIndex][j], i);
+				TileManager.setTileValue(board[i][j], 0);
+				// Don't want to animate an empty tile sliding
+				TileManager.setTilePrevX(board[i][j], j);
+				TileManager.setTilePrevY(board[i][j], i);
+				currIndex += update;
+				tileMoved = true;
+			} else if(currTileVal != 0) {
+				currIndex += update;
+			}
+			// Update i
+			i += update;
+		}
+		
 		return tileMoved;
 	}
 	
